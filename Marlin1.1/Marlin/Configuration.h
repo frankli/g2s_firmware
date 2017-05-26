d00b54752833114fd36736acb68237fef306317c
Frank Li <frankli78@gmail.com>
2017-05-26 17:07:21 +1000

    Clear probe offset

--- Marlin1.1/Marlin/Configuration.h
+++ Marlin1.1/Marlin/Configuration.h
@@ -680,8 +680,8 @@
  *      O-- FRONT --+
  *    (0,0)
  */
-#define X_PROBE_OFFSET_FROM_EXTRUDER -19     // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER -11   // Y offset: -front +behind [the nozzle]
+#define X_PROBE_OFFSET_FROM_EXTRUDER 0     // X offset: -left  +right  [of the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER 0   // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER -4.55  // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
